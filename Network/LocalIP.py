0d3c7a36e49346b18ed68cebf05e968aa39d1e59
perkomvik <perkomvik@github.com>
2018-02-17 18:08:42 +0100

    hei

---
+++ Network/LocalIP.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+__author__      = "gitgudd"
+
+import socket
+
+def localIP():
+
+	import socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("8.8.8.8", 80))
+	IP = s.getsockname()[0]
+	print IP
+	s.close()
+	return IP
