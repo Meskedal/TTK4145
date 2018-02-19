272a809c52e398b4ed7b607785103557597ae119
Per Kvinnesland Omvik <perkom@stud.ntnu.no>
2018-02-19 16:10:07 +0100

    Heartbeat done

--- Network/LocalIP.py
+++ Network/LocalIP.py
@@ -4,9 +4,8 @@ __author__      = "gitgudd"
 
 import socket
 
-def localIP():
+def local_ip():
 
-	import socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.connect(("8.8.8.8", 80))
 	IP = s.getsockname()[0]
--- Network/UDP.py
+++ Network/UDP.py
@@ -6,20 +6,21 @@ __author__      = "gitgudd"
 
 import socket
 
-def udp_heartbeat_send(ip, port, message):
+def udp_broadcast_heartbeat(port):
 
-	target_ip = ip
-	target_port = port	
+	target_ip = '127.0.0.1'
+	target_port = port
+	message = 'hei'
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	sock.sendto(message, (target_ip, target_port))
 
-def udp_receive(port,timeout):
-
+def udp_heartbeat_receive(port,queue,timeout):
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 	sock.settimeout(timeout)
 	try:
-    	sock.bind(('localhost', port))
+    	sock.bind(('127.0.0.1', port))
 	except:
     	print 'failure to bind'
     	sock.close()
@@ -27,9 +28,14 @@ def udp_receive(port,timeout):
 
     try:
     	data, addr = sock.recvfrom(1024)
+    except timeout as e:
+    	print e
+    	return False
 
+    queue.put(addr)
 
-def 
+    return True
+
 
 
     
